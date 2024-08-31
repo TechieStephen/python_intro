29c8e2e3513e1422d56eb9e9d9e8a25e694cb5f9
stephen eduke <stephen.eduke@vnicomhub.com>
2024-08-31 11:59:13 +0100

    input in pythhon

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 # Version control system
-print("Hello World")
-print("Stephen")
+print("Enter First Number:")
+first_num = input()
+print("Enter Second Number:")
+second_num = input()
+
+sum = int(first_num) + int(second_num)
+print("answer is: " + str(sum))
