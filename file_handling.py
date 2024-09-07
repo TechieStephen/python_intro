dec2d95732295c72fc10376e1a5413b8c3ebc399
stephen eduke <stephen.eduke@vnicomhub.com>
2024-09-07 12:53:48 +0100

    loops and file handling

---
+++ file_handling.py
@@ -0,0 +1,4 @@
+file = open("students.txt")
+# line1 = file.readline(1)
+
+
---
+++ loop.py
@@ -0,0 +1,17 @@
+# fruits = ['mango', 'pear', 'apple']
+
+# for fruit in fruits:
+#     print(fruit)
+    
+# for x in range(20):
+#     print(x)
+
+adj = ['red', 'big', 'tasty']
+fruits = ['mango', 'pear', 'apple']
+
+for fruit in fruits:
+    for x in adj:
+        print(fruit, x)
+
+
+
